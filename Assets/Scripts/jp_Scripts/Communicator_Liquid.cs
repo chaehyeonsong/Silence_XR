ebfd2855e5c6eb0573aea9619099951e6d8eab79
JP-sunjk99 <sunjk99@snu.ac.kr>
2025-12-14 17:16:20 +0900

    Modifications to make gameplay more friendly

--- Assets/Scripts/jp_Scripts/Communicator_Liquid.cs
+++ Assets/Scripts/jp_Scripts/Communicator_Liquid.cs
@@ -176,7 +176,7 @@ public class Communicator_Liquid : MonoBehaviour
         target_flask.FillInLiquid(0.00001f, Color.white, Color.white);
         target_flask.GetComponent<FlaskCollisionDetector>().Init(gameController);
         //for testing purposes only
-        target_flask.FillInLiquid(0.002f, target, target);
+        //target_flask.FillInLiquid(0.002f, target, target);
     }
 
     public void NowChecking()
